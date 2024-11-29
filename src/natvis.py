15d794869d57de0dcc48fde5d3fbe3bf840334d1
Alexander Volnov <dev.voln@gmail.com>
2024-11-29 20:20:17 +0300

    Fix natvis.py

--- src/natvis.py
+++ src/natvis.py
@@ -309,7 +309,7 @@ def remove_namespace(doc, namespace):
     """Remove namespace in the passed document in place."""
     ns = u'{%s}' % namespace
     nsl = len(ns)
-    for elem in doc.getiterator():
+    for elem in doc.iter():
         if elem.tag.startswith(ns):
             elem.tag = elem.tag[nsl:]
 
